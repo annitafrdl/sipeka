6389ae6bef82a4883f7442ba7ad397cf27e797e3
annitafrdl <faradilaannita@gmail.com>
2022-06-24 17:14:05 +0700

    change to CI

--- DeleteDataUser.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "Koneksi.php";
-$tKode = $_GET['Kode'];
-$query = "DELETE FROM tbl_user WHERE username = '$tKode'"; mysqli_query($connection,$query);
-?>
-<script type="text/javascript"> window.location = "FrmHome.php?page=frmDaftarUser.php";
-</script>
--- footer.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<div id="footer">
-<br>
-<center><font color="#FFA07A" >Copyright &copy; 2022 - Mrs. Annita Faradila</font></center>
-</div>
-</body>
-</html>
